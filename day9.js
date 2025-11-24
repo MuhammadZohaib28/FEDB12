bc74873f6a1e206c3200970d4895f3cedbe080fc
= <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-11-24 22:03:44 +0500

    for loop

---
+++ day9.js
@@ -0,0 +1,13 @@
+// LOOPS
+
+// FOR LOOP
+// WHILE LOOP
+// DO WHILE LOOP
+
+console.log("LOOPS");
+
+// DRY - DON'T REPEAT  YOURSELF
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`SNO: ${i}. Passed`);
+}
