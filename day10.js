33429014ad8c4591c71f7800921a88e4d80505c7
= <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-11-25 22:04:50 +0500

    for loop added

---
+++ day10.js
@@ -0,0 +1,29 @@
+// FOR LOOP PRACTICE
+// TABLE GENERATION
+
+// 5 x 1 = 5
+// 5 x 2 = 10
+// 5 x 3 = 15
+// 5 x 4 = 20
+// 5 x 5 = 25
+// 5 x 6 = 30
+// 5 x 7 = 35
+// 5 x 8 = 40
+// 5 x 9 = 45
+// 5 x 10 = 50
+
+// % - MODULUS
+
+let tableNumber = 7;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${tableNumber} x ${i} = ${tableNumber * i}`);
+}
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(`${i} is a EVEN number`);
+  } else {
+    console.log(`${i} is a ODD number`);
+  }
+}
